b3061d8605e470de721838097a6019b9cd88866d
aleaguilar <aleaguilar@palaceresorts.com>
2023-09-13 12:33:03 -0600

    added schema validation

--- src/modules/management/controller/CreateManagementController/1.0.0/CreateManagementController.ts
+++ src/modules/management/controller/CreateManagementController/1.0.0/CreateManagementController.ts
@@ -1,28 +1,37 @@
 import 'reflect-metadata';
 import TYPES from 'src/TYPES';
+import schema from './schema';
 import { inject, injectable } from 'inversify';
+import { APIGatewayProxyEvent } from 'aws-lambda';
 import Adapter from 'src/infrastructure/adapter/Adapter';
 import Mapper from 'src/infrastructure/domain/mapper/Mapper';
 import TenantByUserDto from 'src/domain/dto/admin/TenantByUserDto';
 import APIGatewayResult from 'src/infrastructure/domain/dto/APIGatewayResult';
-import { controller } from 'src/infrastructure/controller/decorator/controller';
 import APIGatewayProxyEventBaseController from 'src/infrastructure/controller/APIGatewayProxyEventBaseController';
+import Validator, { VALIDATOR_TYPE } from 'src/utils/request/Validator';
 
 @injectable()
-@controller()
 export default class CreateMomentSpaceAdminController extends APIGatewayProxyEventBaseController<TenantByUserDto> {
   constructor(
-    @inject(TYPES.CreateManagementAdapter) private readonly adapter: Adapter<never, Promise<TenantByUserDto>>,
+    @inject(TYPES.CreateManagementAdapter) private readonly adapter: Adapter<string, Promise<TenantByUserDto>>,
     @inject(TYPES.APIGatewayResultMapperService) apiGatewayResultMapperService: Mapper<TenantByUserDto, APIGatewayResult<TenantByUserDto>>
   ) {
     super(apiGatewayResultMapperService);
   }
 
-  protected async validate(): Promise<void> {
+  protected async validate(port: APIGatewayProxyEvent): Promise<void> {
+    const validator = new Validator({
+      schema,
+      event: port,
+      type: VALIDATOR_TYPE.BODY
+    });
+    await validator.execute();
   }
 
-  protected async run(): Promise<TenantByUserDto> {
-    const tenantByUserDto: TenantByUserDto = await this.adapter.execute();
+  protected async run(port: APIGatewayProxyEvent & { body: unknown }): Promise<TenantByUserDto> {
+    // console.log('CreateMomentSpaceAdminController run', port);
+    const { name } = port.body as unknown as { name: string };
+    const tenantByUserDto: TenantByUserDto = await this.adapter.execute(name);
     return tenantByUserDto;
   }
 }
