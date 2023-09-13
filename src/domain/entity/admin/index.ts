73954ece30ec2632adde86dc6fb9e7aad7dc35bd
aleaguilar <aleaguilar@palaceresorts.com>
2023-09-13 12:31:14 -0600

    updated entities

--- src/domain/entity/admin/index.ts
+++ src/domain/entity/admin/index.ts
@@ -2,10 +2,12 @@ import Role from './Role';
 import User from './User';
 import Tenant from './Tenant';
 import TenantByUser from './TenantByUser';
+import DataBaseUser from './DataBaseUser';
 
 export default {
   User,
   Role,
   Tenant,
-  TenantByUser
+  TenantByUser,
+  DataBaseUser,
 }
